91d1bbc88f14b14864064f67b112e293246f50aa
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-04-17 17:36:00 -0400

    Finished hackathon

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports.curry = fn => {
-  return function (...arguments){
-    if (arguments.length>= fn.length) return fn(...arguments)
+const curry = fn => {
+  return function (...arg){
+    if (arg.length>= fn.length) return fn(...arg)
     else{
-      const partialFn = fb.bind(null,...arguments)
-      return this.curry(partialFn)
+      const partialFn = fn.bind(null,...arg)
+      return curry(partialFn)
     }
   }
 }
+
+module.exports = curry
--- test/curry-test.js
+++ test/curry-test.js
@@ -4,7 +4,14 @@ let a = require("assert")
 describe('curry', function(){
 
   it('should curry in the haskell sense, taking the arity from the function', function(){
-      const add = function(a,b) => {a+b};
+      const add = (a,b) => a + b
       const curryAdd = curry(add);
-      a.equal(add(1, 2), curryAdd((1)(2));
+      a.equal(add(1, 2), curryAdd(1)(2));
   });
+
+  it('can handle multiple parameters', function(){
+    const add = (a,b,c,d,e) => a + b + c +d + e
+    const curryAdd = curry(add);
+    a.equal(add(1, 2,3,4,5), curryAdd(1)(2)(3)(4)(5));
+});
+})
